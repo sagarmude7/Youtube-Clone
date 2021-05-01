4d9c225ebb0e50604bacfe700f37390b14c727ec
Sagar Mude <sagarmude7@gmail.com>
2021-05-01 14:01:30 +0530

    deployment

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,63 +1,59 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
-   addComment,
-   getCommentsOfVideoById,
-} from '../../redux/actions/comments.action'
-import Comment from '../comment/Comment'
-import './_comments.scss'
+  addComment,
+  getCommentsOfVideoById,
+} from "../../redux/actions/comments.action";
+import Comment from "../comment/Comment";
+import "./_comments.scss";
 const Comments = ({ videoId, totalComments }) => {
-   const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-   useEffect(() => {
-      dispatch(getCommentsOfVideoById(videoId))
-   }, [videoId, dispatch])
+  useEffect(() => {
+    dispatch(getCommentsOfVideoById(videoId));
+  }, [videoId, dispatch]);
 
-   const comments = useSelector(state => state.commentList.comments)
+  const comments = useSelector((state) => state.commentList.comments);
 
-   const [text, setText] = useState('')
+  const [text, setText] = useState("");
 
-   const _comments = comments?.map(
-      comment => comment.snippet.topLevelComment.snippet
-   )
+  const _comments = comments?.map(
+    (comment) => comment.snippet.topLevelComment.snippet
+  );
 
-   const handleComment = e => {
-      e.preventDefault()
-      if (text.length === 0) return
+  const handleComment = (e) => {
+    e.preventDefault();
+    if (text.length === 0) return;
 
-      dispatch(addComment(videoId, text))
+    dispatch(addComment(videoId, text));
 
-      setText('')
-   }
-   const user = useSelector(state => state.auth?.user)
+    setText("");
+  };
+  const { photoURL } = useSelector((state) => state.auth?.user);
 
-   return (
-      <div className='comments'>
-         <p>{totalComments} Comments</p>
-         <div className='my-2 comments__form d-flex w-100'>
-            <img
-               src={user?.photoURL}
-               alt='avatar'
-               className='mr-3 rounded-circle'
-            />
-            <form onSubmit={handleComment} className='d-flex flex-grow-1'>
-               <input
-                  type='text'
-                  className='flex-grow-1'
-                  placeholder='Write a comment...'
-                  value={text}
-                  onChange={e => setText(e.target.value)}
-               />
-               <button className='p-2 border-0'>Comment</button>
-            </form>
-         </div>
-         <div className='comments__list'>
-            {_comments?.map((comment, i) => (
-               <Comment comment={comment} key={i} />
-            ))}
-         </div>
+  return (
+    <div className="comments">
+      <p>{totalComments} Comments</p>
+      <div className="my-2 comments__form d-flex w-100">
+        <img src={photoURL} alt="avatar" className="mr-3 rounded-circle" />
+        <form onSubmit={handleComment} className="d-flex flex-grow-1">
+          <input
+            type="text"
+            className="flex-grow-1"
+            placeholder="Write a comment..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button className="p-2 border-0">Comment</button>
+        </form>
       </div>
-   )
-}
+      <div className="comments__list">
+        {_comments?.map((comment, i) => (
+          <Comment comment={comment} key={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Comments
+export default Comments;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,63 +1,57 @@
-import React, { useState } from 'react'
-import './_header.scss'
+import React, { useState } from "react";
+import "./_header.scss";
 
-import { FaBars } from "react-icons/fa"
-import { AiOutlineSearch } from "react-icons/ai"
-import { MdNotifications, MdApps } from "react-icons/md"
-import { useHistory } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { FaBars } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdNotifications, MdApps } from "react-icons/md";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-const Header = ({handleToggleSidebar}) => {
+const Header = ({ handleToggleSidebar }) => {
+  const [input, setInput] = useState("");
 
-   const [input, setInput] = useState('')
+  const history = useHistory();
 
-   const history = useHistory()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-   const handleSubmit = e => {
-      e.preventDefault()
+    history.push(`/search/${input}`);
+  };
+  const { photoURL } = useSelector((state) => state.auth?.user);
 
-      history.push(`/search/${input}`)
-   }
-   const user = useSelector(state => state.auth?.user)
+  return (
+    <div className="header">
+      <FaBars
+        className="header__menu"
+        size={26}
+        onClick={() => handleToggleSidebar()}
+      />
 
+      <img
+        src="http://pngimg.com/uploads/youtube/youtube_PNG2.png"
+        alt=""
+        className="header__logo"
+      />
 
-    return (
-        <div className="header">
-            
-          <FaBars 
-          className="header__menu" 
-          size={26} 
-          onClick={() => handleToggleSidebar()}
-          />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">
+          <AiOutlineSearch size={22} />
+        </button>
+      </form>
 
-          <img src="http://pngimg.com/uploads/youtube/youtube_PNG2.png"
-            alt=""
-            className="header__logo"
-         />
+      <dic className="header__icons">
+        <MdNotifications size={28} />
+        <MdApps size={28} />
+        <img src={photoURL} alt="avatar" />
+      </dic>
+    </div>
+  );
+};
 
-         <form onSubmit={handleSubmit}>
-         <input
-               type='text'
-               placeholder='Search'
-               value={input}
-               onChange={e => setInput(e.target.value)}
-            />
-            <button type="submit">
-               <AiOutlineSearch size={22} />
-            </button>
-         </form>
-
-         <dic className="header__icons">
-            <MdNotifications size={28} />
-            <MdApps size={28} />
-            <img
-               src={user?.photoURL}
-               alt="avatar"
-            />
-         </dic>
-        </div>
-    )
-}
-
-export default Header
-
+export default Header;
