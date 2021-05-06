d5b859f69f0d08b0acb10e29843e486058a829e7
Aditya <69779675+Adi1505-macintosh@users.noreply.github.com>
2021-05-06 10:22:17 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,8 +6,8 @@ const firebaseConfig = {
   authDomain: "osc-project-sadtube.firebaseapp.com",
   projectId: "osc-project-sadtube",
   storageBucket: "osc-project-sadtube.appspot.com",
-  messagingSenderId: "129967381680",
-  appId: "1:129967381680:web:8f981192712f1d1d296f75",
+  messagingSenderId: "1080097038787",
+  appId: "1:1080097038787:web:8f981192712f1d1d296f75",
 };
 
 firebase.initializeApp(firebaseConfig);
