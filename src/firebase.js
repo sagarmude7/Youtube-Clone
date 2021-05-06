94c61d67c821aa552302e1322c62b6f5b512a441
Aditya <69779675+Adi1505-macintosh@users.noreply.github.com>
2021-05-06 09:54:43 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVSflRM4mfVtI36LuW8wBNH_CaFbk5hDU",
+  apiKey: "AIzaSyBxOcZHZdzKRi6TG6C8zSFIjdYjfsi9K_g",
   authDomain: "osc-project-sadtube.firebaseapp.com",
   projectId: "osc-project-sadtube",
   storageBucket: "osc-project-sadtube.appspot.com",
   messagingSenderId: "129967381680",
   appId: "1:129967381680:web:8f981192712f1d1d296f75",
-  measurementId: "G-QJL1S7G521",
+  measurementId: "G-QJL1S7G521"
 };
 
 firebase.initializeApp(firebaseConfig);
